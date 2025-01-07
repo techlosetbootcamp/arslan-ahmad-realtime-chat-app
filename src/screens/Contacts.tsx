400d0eac9b6317788650a76bc068a57e95396582
ARslan_Ahm <mycomputer2214@gmail.com>
2025-01-07 16:47:17 +0500

    [feat] Added react-navigation library.

---
+++ src/screens/Contacts.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Contacts = () => {
+  return (
+    <View>
+      <Text style={{color: "white"}}>Contacts</Text>
+    </View>
+  )
+}
+
+export default Contacts
+
+const styles = StyleSheet.create({})
