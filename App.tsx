61110dcb2ea4fa3b2b22855a270b73c8f27b3ec2
ARslan_Ahm <mycomputer2214@gmail.com>
2025-01-13 21:27:07 +0500

    [feat] Just added Splash screen into my app.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider, useDispatch } from 'react-redux';
 import { store } from './src/store/store';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -7,10 +7,12 @@ import { getUserFromStorage } from './src/services/authHelpers';
 import { setLoading, setUser } from './src/store/slices/userSlice';
 import useAuth from './src/hook/useAuth';
 import Header from './src/components/Header';
+import SplashScreen from 'react-native-splash-screen';
 
 const AppContent = () => {
   const dispatch = useDispatch();
-  const {user} = useAuth();
+  const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkUserSession = async () => {
@@ -19,11 +21,22 @@ const AppContent = () => {
         dispatch(setUser(storedUser));
       }
       dispatch(setLoading(false));
+      setIsLoading(false);  // Indicate that loading is complete
+      SplashScreen.hide();  // Hide splash screen when the app is ready
     };
     checkUserSession();
   }, [dispatch]);
 
-  return user?.email && user.uid ? <Header><Navigation /></Header> : <Navigation />;
+  return (
+    <>
+      {isLoading ? (
+        // You can add a custom loader here if needed.
+        null
+      ) : (
+        user?.email && user.uid ? <Header><Navigation /></Header> : <Navigation />
+      )}
+    </>
+  );
 };
 
 const App = () => {
--- android/app/src/main/java/com/realtimechatapp/MainActivity.kt
+++ android/app/src/main/java/com/realtimechatapp/MainActivity.kt
@@ -6,6 +6,9 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import android.os.Bundle;
+Import org.devio.rn.splashscreen.Splashscreen;
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -15,7 +18,8 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "RealtimeChatApp"
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
+      SplashScreen.show(this)
+      super.onCreate(savedInstanceState)
   }
 
   /**
